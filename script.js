0ed96f565f3814a8067e5f82c33bba227ebf36f8
jbusomm <145897540+jbusomm@users.noreply.github.com>
2023-12-28 13:02:55 +0100

    Add operation logic

--- script.js
+++ script.js
@@ -14,3 +14,38 @@ function divide(a, b) {
   if (a === 0 || b === 0) alert("Can't divide by 0! Try again.");
   else return a / b;
 }
+
+let firstNumber;
+let secondNumber;
+let operator;
+
+function operation() {
+  let inp = prompt("Prease, calculation?");
+  if (inp.includes("+")) {
+    return calculation(inp, "+");
+  }
+  if (inp.includes("-")) {
+    return calculation(inp, "-");
+  }
+  if (inp.includes("*")) {
+    return calculation(inp, "*");
+  }
+  if (inp.includes("/")) {
+    return calculation(inp, "/");
+  }
+}
+
+function calculation(inp, op) {
+  let numbers = inp.split(op);
+  firstNumber = Number(numbers[0]);
+  secondNumber = Number(numbers[1]);
+  if (op === "+") {
+    return add(firstNumber, secondNumber);
+  } else if (op === "-") {
+    return sub(firstNumber, secondNumber);
+  } else if (op === "*") {
+    return multi(firstNumber, secondNumber);
+  } else if (op === "/") {
+    return divide(firstNumber, secondNumber);
+  }
+}
