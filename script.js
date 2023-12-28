098e0b1061b9315e005b63fc5840d1e4ebc0fb5f
jbusomm <145897540+jbusomm@users.noreply.github.com>
2023-12-28 11:24:31 +0100

    Add basic functions for all the basic math operations

---
+++ script.js
@@ -0,0 +1,16 @@
+function add(a, b) {
+  return a + b;
+}
+
+function sub(a, b) {
+  return a - b;
+}
+
+function multi(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (a === 0 || b === 0) alert("Can't divide by 0! Try again.");
+  else return a / b;
+}
