50b15dd891b8b5910619a94277dd81817111900b
jbusomm <145897540+jbusomm@users.noreply.github.com>
2024-01-03 12:41:21 +0100

    Created functions that populate the display when pressing the numbers

--- script.js
+++ script.js
@@ -50,6 +50,138 @@ function calculation(inp, op) {
   }
 }
 
+let displayArr = [];
+
 const numDisplay = document.querySelector(".num-display");
 
-const numOne = document.querySelector;
+const numOne = document.querySelector(".num-one");
+const numTwo = document.querySelector(".num-two");
+const numThree = document.querySelector(".num-three");
+const numFour = document.querySelector(".num-four");
+const numFive = document.querySelector(".num-five");
+const numSix = document.querySelector(".num-six");
+const numSeven = document.querySelector(".num-seven");
+const numEight = document.querySelector(".num-eight");
+const numNine = document.querySelector(".num-nine");
+const numZero = document.querySelector(".num-zero");
+const addBtn = document.querySelector(".add-btn");
+const minusBtn = document.querySelector(".minus-btn");
+const multiBtn = document.querySelector(".multi-btn");
+const divideBtn = document.querySelector(".divide-btn");
+const clearBtn = document.querySelector(".clear-btn");
+const equalsBtn = document.querySelector(".equals-btn");
+
+numOne.addEventListener("click", () => {
+  displayArr.push("1");
+  numDisplay.textContent = displayArr.join("");
+});
+
+numTwo.addEventListener("click", () => {
+  displayArr.push("2");
+  numDisplay.textContent = displayArr.join("");
+});
+
+numThree.addEventListener("click", () => {
+  displayArr.push("3");
+  numDisplay.textContent = displayArr.join("");
+});
+
+numFour.addEventListener("click", () => {
+  displayArr.push("4");
+  numDisplay.textContent = displayArr.join("");
+});
+
+numFive.addEventListener("click", () => {
+  displayArr.push("5");
+  numDisplay.textContent = displayArr.join("");
+});
+
+numSix.addEventListener("click", () => {
+  displayArr.push("6");
+  numDisplay.textContent = displayArr.join("");
+});
+
+numSeven.addEventListener("click", () => {
+  displayArr.push("7");
+  numDisplay.textContent = displayArr.join("");
+});
+
+numEight.addEventListener("click", () => {
+  displayArr.push("8");
+  numDisplay.textContent = displayArr.join("");
+});
+
+numNine.addEventListener("click", () => {
+  displayArr.push("9");
+  numDisplay.textContent = displayArr.join("");
+});
+
+numZero.addEventListener("click", () => {
+  displayArr.push("0");
+  numDisplay.textContent = displayArr.join("");
+});
+
+addBtn.addEventListener("click", () => {
+  let lastItem = displayArr[displayArr.length - 1];
+  if (
+    lastItem === "+" ||
+    lastItem === "-" ||
+    lastItem === "x" ||
+    lastItem === "/"
+  ) {
+    console.error("error, can't have two signs next to each other");
+  } else {
+    displayArr.push("+");
+    numDisplay.textContent = displayArr.join("");
+  }
+});
+
+minusBtn.addEventListener("click", () => {
+  let lastItem = displayArr[displayArr.length - 1];
+  if (
+    lastItem === "+" ||
+    lastItem === "-" ||
+    lastItem === "x" ||
+    lastItem === "/"
+  ) {
+    console.error("error, can't have two signs next to each other");
+  } else {
+    displayArr.push("-");
+    numDisplay.textContent = displayArr.join("");
+  }
+});
+
+multiBtn.addEventListener("click", () => {
+  let lastItem = displayArr[displayArr.length - 1];
+  if (
+    lastItem === "+" ||
+    lastItem === "-" ||
+    lastItem === "x" ||
+    lastItem === "/"
+  ) {
+    console.error("error, can't have two signs next to each other");
+  } else {
+    displayArr.push("x");
+    numDisplay.textContent = displayArr.join("");
+  }
+});
+
+divideBtn.addEventListener("click", () => {
+  let lastItem = displayArr[displayArr.length - 1];
+  if (
+    lastItem === "+" ||
+    lastItem === "-" ||
+    lastItem === "x" ||
+    lastItem === "/"
+  ) {
+    console.error("error, can't have two signs next to each other");
+  } else {
+    displayArr.push("/");
+    numDisplay.textContent = displayArr.join("");
+  }
+});
+
+clearBtn.addEventListener("click", () => {
+  displayArr = [];
+  numDisplay.textContent = displayArr;
+});
