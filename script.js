a9721abecd03fcfc67fa86e5041f16082bbd0dfb
jbusomm <145897540+jbusomm@users.noreply.github.com>
2024-01-09 17:18:51 +0100

    Make calc logic and display work with addition

--- script.js
+++ script.js
@@ -19,9 +19,9 @@ let firstNumber;
 let secondNumber;
 let operator;
 
-function operation() {
-  let inp = prompt("Prease, calculation?");
+function operation(inp) {
   if (inp.includes("+")) {
+    console.log(`does this even work ${inp}`);
     return calculation(inp, "+");
   }
   if (inp.includes("-")) {
@@ -39,6 +39,10 @@ function calculation(inp, op) {
   let numbers = inp.split(op);
   firstNumber = Number(numbers[0]);
   secondNumber = Number(numbers[1]);
+  console.log(`this is numbers ${numbers}`);
+  console.log(op);
+  console.log(firstNumber);
+  console.log(secondNumber);
   if (op === "+") {
     return add(firstNumber, secondNumber);
   } else if (op === "-") {
@@ -51,6 +55,10 @@ function calculation(inp, op) {
 }
 
 let displayArr = [];
+let resultBuffer = [];
+let currOp = "";
+let currNum = [];
+let currSecondNum = [];
 
 const numDisplay = document.querySelector(".num-display");
 
@@ -73,51 +81,71 @@ const equalsBtn = document.querySelector(".equals-btn");
 
 numOne.addEventListener("click", () => {
   displayArr.push("1");
+  if (currOp === "") currNum.push("1");
+  else if (currOp != "") currSecondNum.push("1");
   numDisplay.textContent = displayArr.join("");
 });
 
 numTwo.addEventListener("click", () => {
   displayArr.push("2");
+  if (currOp === "") currNum.push("2");
+  else if (currOp != "") currSecondNum.push("2");
   numDisplay.textContent = displayArr.join("");
 });
 
 numThree.addEventListener("click", () => {
   displayArr.push("3");
+  if (currOp === "") currNum.push("3");
+  else if (currOp != "") currSecondNum.push("3");
   numDisplay.textContent = displayArr.join("");
 });
 
 numFour.addEventListener("click", () => {
   displayArr.push("4");
+  if (currOp === "") currNum.push("4");
+  else if (currOp != "") currSecondNum.push("4");
   numDisplay.textContent = displayArr.join("");
 });
 
 numFive.addEventListener("click", () => {
   displayArr.push("5");
+  if (currOp === "") currNum.push("5");
+  else if (currOp != "") currSecondNum.push("5");
   numDisplay.textContent = displayArr.join("");
 });
 
 numSix.addEventListener("click", () => {
   displayArr.push("6");
+  if (currOp === "") currNum.push("6");
+  else if (currOp != "") currSecondNum.push("6");
   numDisplay.textContent = displayArr.join("");
 });
 
 numSeven.addEventListener("click", () => {
   displayArr.push("7");
+  if (currOp === "") currNum.push("7");
+  else if (currOp != "") currSecondNum.push("7");
   numDisplay.textContent = displayArr.join("");
 });
 
 numEight.addEventListener("click", () => {
   displayArr.push("8");
+  if (currOp === "") currNum.push("8");
+  else if (currOp != "") currSecondNum.push("8");
   numDisplay.textContent = displayArr.join("");
 });
 
 numNine.addEventListener("click", () => {
   displayArr.push("9");
+  if (currOp === "") currNum.push("9");
+  else if (currOp != "") currSecondNum.push("9");
   numDisplay.textContent = displayArr.join("");
 });
 
 numZero.addEventListener("click", () => {
   displayArr.push("0");
+  if (currOp === "") currNum.push("0");
+  else if (currOp != "") currSecondNum.push("9");
   numDisplay.textContent = displayArr.join("");
 });
 
@@ -131,8 +159,24 @@ addBtn.addEventListener("click", () => {
   ) {
     console.error("error, can't have two signs next to each other");
   } else {
-    displayArr.push("+");
-    numDisplay.textContent = displayArr.join("");
+    if (currOp === "+" || currOp === "-" || currOp === "x" || currOp === "/") {
+      resultBuffer.push(currNum.join(""));
+      resultBuffer.push(currOp);
+      resultBuffer.push(currSecondNum.join(""));
+      currSecondNum = [];
+      currNum = [];
+      currNum.push(operation(resultBuffer.join("")));
+      console.log(resultBuffer.join(""));
+      console.log(currNum);
+      resultBuffer = [];
+      currOp = "+";
+      displayArr.push("+");
+      numDisplay.textContent = displayArr.join("");
+    } else {
+      currOp = "+";
+      displayArr.push("+");
+      numDisplay.textContent = displayArr.join("");
+    }
   }
 });
 
